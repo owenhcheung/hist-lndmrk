8c68caa94ab9f7b13711498d02d012e4ad695c2f
Owen <ocheung198@gmail.com>
2022-12-01 23:13:46 -0800

    sss

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script>
   import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import Map from './lib/map.svelte'
 </script>
 
 <main>
@@ -12,19 +12,10 @@
       <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
     </a>
   </div>
-  <h1>Vite + Svelte</h1>
 
   <div class="card">
-    <Counter />
+    <Map />
   </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
---
+++ src/lib/map.svelte
@@ -0,0 +1,39 @@
+<script>
+	import { onMount } from 'svelte';
+	import * as d3 from 'd3';
+  var data = [30, 144, 168, 281, 303, 365];
+	var data1 = d3.csv("./data/unafford_housing2011_2015.csv");
+  var data2 = d3.csv("./data/unafford_housing2015_2015.csv");
+  var land_data = d3.csv("./data/landmarks.csv");
+
+  console.log(data1);
+
+	let el;
+
+	onMount(() => {
+		d3.select(el)
+			.selectAll("div")
+			.data(data)
+			.enter()
+			.append("div")
+			.style("width", function(d) {
+				return d + "px";
+			})
+			.text(function(d) {
+				return d;
+			});
+	});
+</script>
+
+<style>
+	.chart :global(div) {
+		font: 10px sans-serif;
+		background-color: steelblue;
+		text-align: right;
+		padding: 3px;
+		margin: 1px;
+		color: white;
+	}
+</style>
+
+<div bind:this={el} class="chart"></div>
