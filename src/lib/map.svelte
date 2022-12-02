f46dc4e15c91ebd9f4f557b87c8dbd8a0d643b4c
Simon <fujizhang2525@gmail.com>
2022-12-02 03:40:00 -0500

    onmount

--- src/lib/map.svelte
+++ src/lib/map.svelte
@@ -2,15 +2,14 @@
 	import { onMount } from 'svelte';
 	import * as d3 from 'd3';
   var data = [30, 144, 168, 281, 303, 365];
-	var data1 = d3.csv("./data/unafford_housing2011_2015.csv");
-  var data2 = d3.csv("./data/unafford_housing2015_2015.csv");
-  var land_data = d3.csv("./data/landmarks.csv");
+	let data1
+  //var data2 = d3.csv("./data/unafford_housing2015_2015.csv");
+  //var land_data = d3.csv("./data/landmarks.csv");
 
-  console.log(data1);
+  console.log();
 
 	let el;
-
-	onMount(() => {
+	function map() {
 		d3.select(el)
 			.selectAll("div")
 			.data(data)
@@ -22,6 +21,14 @@
 			.text(function(d) {
 				return d;
 			});
+	}
+
+	onMount(async () => {
+		data1 = await d3.csv("./unafford_housing2011_2015.csv");
+		const test = data1.filter(d=> d["County Name"] === "King")
+		console.log(test)
+			
+			
 	});
 </script>
 
